3d7ebf666a0ace5151168969311b9ced51411a58
Boston Hsu <bostonhsu@outlook.com>
2017-06-03 10:13:32 +0800

    把计算常客积分抽象为函数，并移动到类Rental里。

--- src/cn/windssoft/test/Rental.java
+++ src/cn/windssoft/test/Rental.java
@@ -38,4 +38,13 @@ public class Rental {
 		}
 		return result;
 	}
+
+	public int getFrequentRenterPoints() {
+		int result = 1;
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+				getDaysRented() > 1) {
+			result ++;
+		}
+		return result;
+	}
 }
