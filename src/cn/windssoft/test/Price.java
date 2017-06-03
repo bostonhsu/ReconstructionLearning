0727baffa2ecdc79a5de2274b9ebc72c8525e029
Boston Hsu <bostonhsu@outlook.com>
2017-06-03 11:38:27 +0800

    给类RegularPrice添加函数getCharge，覆盖父类的getCharge函数。但是并不使用到超类Price里。

--- src/cn/windssoft/test/Price.java
+++ src/cn/windssoft/test/Price.java
@@ -45,4 +45,12 @@ class RegularPrice extends Price {
 	int getPriceCode() {
 		return Movie.REGULAR;
 	}
+	
+	public double getCharge(int daysRented) {
+		double result = 2;
+		if (daysRented > 2) {
+			result += (daysRented - 2) * 1.5;
+		}
+		return result;
+	}
 }
