4d17d5428aff833bba83b183e402767196c41460
Boston Hsu <bostonhsu@outlook.com>
2017-06-09 16:19:21 +0800

    为replace temp with query准备代码。

--- src/cn/windssoft/sixth/First.java
+++ src/cn/windssoft/sixth/First.java
@@ -16,6 +16,23 @@ public class First {
         printDetails(outstanding);
     }
 
+    public void printOwingNew(double amount) {
+        Enumeration e = _orders.elements();
+        double outstanding = 0.0;
+
+        System.out.println("*************************");
+        System.out.println("****** Customer Owes*****");
+        System.out.println("*************************");
+
+        while (e.hasMoreElements()) {
+            Order each = (Order)e.nextElement();
+            outstanding += each.getAmount();
+        }
+
+        System.out.println("name:" + _name);
+        System.out.println("amount" + amount);
+    }
+    
     public void printOwing(double previousAmount) {
         printBanner();
         double outstanding = getOutstandingNew(previousAmount * 1.2);
