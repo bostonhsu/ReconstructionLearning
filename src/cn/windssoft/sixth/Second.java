b988444a3ce24691d80367ccd2594f3c0f8ce995
Boston Hsu <bostonhsu@outlook.com>
2017-06-10 08:22:25 +0800

    prepare

--- src/cn/windssoft/sixth/Second.java
+++ src/cn/windssoft/sixth/Second.java
@@ -6,11 +6,29 @@ package cn.windssoft.sixth;
 public class Second {
     double _height;
     double _width;
+    private double _primaryForce;
+    private double secondaryForce;
+    private double _mass;
+    private int _delay;
 
     private void test() {
         final double perimeter = 2 * (_height + _width);
         System.out.println(perimeter);
         final double area = _height * _width;
         System.out.println(area);
+    }
+
+    double getDistanceTravelled(int time) {
+        double result;
+        double acc = _primaryForce / _mass;
+        int primaryTime = Math.min(time, _delay);
+        result = 0.5 * acc * primaryTime * primaryTime;
+        int secondaryTime = time - _delay;
+        if (secondaryTime > 0) {
+            double primaryVel = acc * _delay;
+            acc = (_primaryForce + secondaryForce) / _mass;
+            result += primaryVel * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+        }
+        return result;
     }
 }
