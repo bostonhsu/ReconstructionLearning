ec41356e3b0982e0c6e6f18fe707d8b08bf31e1d
Boston Hsu <bostonhsu@outlook.com>
2017-06-10 08:45:58 +0800

    不对参数赋值。

--- src/cn/windssoft/sixth/Second.java
+++ src/cn/windssoft/sixth/Second.java
@@ -12,10 +12,11 @@ public class Second {
     private int _delay;
 
     int discount(int inputVal, int quantity, int yearToDate) {
-        if (inputVal > 50) inputVal -= 2;
-        if (quantity > 100) inputVal -= 1;
-        if (yearToDate > 10000) inputVal -= 4;
-        return inputVal;
+        int result = inputVal;
+        if (inputVal > 50) result -= 2;
+        if (quantity > 100) result -= 1;
+        if (yearToDate > 10000) result -= 4;
+        return result;
     }
 
     private void test() {
