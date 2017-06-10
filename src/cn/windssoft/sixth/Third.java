026fb4e394befcdc6b9a37b21c947abccec4bd2f
Boston Hsu <bostonhsu@outlook.com>
2017-06-10 10:13:23 +0800

    done

--- src/cn/windssoft/sixth/Third.java
+++ src/cn/windssoft/sixth/Third.java
@@ -1,19 +1,17 @@
 package cn.windssoft.sixth;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/6/10.
  */
 public class Third {
     String foundPerson(String[] people) {
+        List candidates = Arrays.asList(new String[] {"Don", "John", "kent"});
         for (int i = 0; i < people.length; i++) {
-            if (people[i].equals("Don")) {
-                return "Don";
-            }
-            if (people[i].equals("John")) {
-                return "John";
-            }
-            if (people[i].equals("Kent")) {
-                return "Kent";
+            if (candidates.contains(people[i])) {
+                return people[i];
             }
         }
         return "";
