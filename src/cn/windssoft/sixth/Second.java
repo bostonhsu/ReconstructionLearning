aa3002c3b30a98169ac818ad59bc8fc2358265a5
Boston Hsu <bostonhsu@outlook.com>
2017-06-10 08:43:58 +0800

    prepare 6.7.1

--- src/cn/windssoft/sixth/Second.java
+++ src/cn/windssoft/sixth/Second.java
@@ -12,11 +12,10 @@ public class Second {
     private int _delay;
 
     int discount(int inputVal, int quantity, int yearToDate) {
-        int result = inputVal;
-        if (inputVal > 50) {
-            result -= 2;
-        }
-        return result;
+        if (inputVal > 50) inputVal -= 2;
+        if (quantity > 100) inputVal -= 1;
+        if (yearToDate > 10000) inputVal -= 4;
+        return inputVal;
     }
 
     private void test() {
