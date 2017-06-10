0e55dac789bf81a4473b9756277d41b45ab0ea29
Boston Hsu <bostonhsu@outlook.com>
2017-06-10 09:59:22 +0800

    函数对象step3

--- src/cn/windssoft/sixth/Account.java
+++ src/cn/windssoft/sixth/Account.java
@@ -14,7 +14,7 @@ public class Account {
         return importantValue3 - 2 * importantValue1;
     }
 
-    private int delta() {
+    int delta() {
         return 0;
     }
 }
@@ -33,5 +33,15 @@ class Gamma {
         this.inputValue = inputValue;
         this.quantity = quantity;
         this.yearToDate = yearToDate;
+    }
+
+    int compute() {
+        int importantValue1 = (inputValue * quantity) + _account.delta();
+        int importantValue2 = (inputValue * yearToDate) + 100;
+        if ((yearToDate - importantValue1) > 100) {
+            importantValue2 -= 20;
+        }
+        int importantValue3 = importantValue2 * 7;
+        return importantValue3 - 2 * importantValue1;
     }
 }
