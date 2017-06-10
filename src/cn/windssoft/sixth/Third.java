c06b6b77bda13da30fa94759a0c8dcaab45aa6fd
Boston Hsu <bostonhsu@outlook.com>
2017-06-10 10:05:56 +0800

    为了把函数本体替换成一个更好的算法做准备。

---
+++ src/cn/windssoft/sixth/Third.java
@@ -0,0 +1,21 @@
+package cn.windssoft.sixth;
+
+/**
+ * Created by Administrator on 2017/6/10.
+ */
+public class Third {
+    String foundPerson(String[] people) {
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].equals("Don")) {
+                return "Don";
+            }
+            if (people[i].equals("John")) {
+                return "John";
+            }
+            if (people[i].equals("Kent")) {
+                return "Kent";
+            }
+        }
+        return "";
+    }
+}
