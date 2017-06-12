1151ffac4bf5e24b183b9d46b058ca82887031ba
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 10:10:31 +0800

    prepare Move Method among classes.

---
+++ src/cn/windssoft/seventh/Account.java
@@ -0,0 +1,25 @@
+package cn.windssoft.seventh;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Account {
+    private AccountType _type;
+    private int _daysOverdrawn;
+
+    double overdraftCharge() {
+        if (_type.isPremium()) {
+            double result = 10;
+            if (_daysOverdrawn > 7) result += (_daysOverdrawn - 7) * 0.85;
+            return result;
+        } else {
+            return _daysOverdrawn * 1.75;
+        }
+    }
+
+    double bankCharge() {
+        double result = 4.5;
+        if (_daysOverdrawn > 0) result +=overdraftCharge();
+        return result;
+    }
+}
---
+++ src/cn/windssoft/seventh/AccountType.java
@@ -0,0 +1,12 @@
+package cn.windssoft.seventh;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class AccountType {
+
+    public boolean isPremium() {
+        boolean result  = false;
+        return result;
+    }
+}
