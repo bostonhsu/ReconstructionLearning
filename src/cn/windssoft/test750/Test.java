7ac2d0ccc22198f27ec36887b397cfa7614ee9d9
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 14:19:41 +0800

    Prepare 7.7.

--- src/cn/windssoft/test750/Test.java
+++ src/cn/windssoft/test750/Test.java
@@ -1,5 +1,7 @@
 package cn.windssoft.test750;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2017/6/12.
  */
@@ -16,5 +18,10 @@ public class Test {
 
     public static void main(String[] args) {
         test();
+    }
+
+    public static void test1() {
+        Date previousEnd = new Date();
+        Date newStart = new Date(previousEnd.getYear(), previousEnd.getMonth(), previousEnd.getDay() + 1);
     }
 }
