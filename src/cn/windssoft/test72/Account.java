b9139e11cbaf08c716ccb0bc436dc25abf452995
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 10:36:02 +0800

    prepare7.1

---
+++ src/cn/windssoft/test72/Account.java
@@ -0,0 +1,13 @@
+package cn.windssoft.test72;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Account {
+    private AccountType _type;
+    private double _interestRate;
+
+    double interestForAmount_days(double amount, int days) {
+        return _interestRate * amount * days / 365;
+    }
+}
---
+++ src/cn/windssoft/test72/AccountType.java
@@ -0,0 +1,8 @@
+package cn.windssoft.test72;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class AccountType {
+    
+}
