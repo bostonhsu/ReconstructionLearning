f0beb50ffa48372a9d02110b97319ec878a96a2f
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 17:34:18 +0800

    next

--- src/cn/windssoft/test810/Perfermance.java
+++ src/cn/windssoft/test810/Perfermance.java
@@ -4,7 +4,7 @@ package cn.windssoft.test810;
  * Created by Administrator on 2017/6/12.
  */
 public class Perfermance {
-    public String[] _data = new String[3];
+    private String[] _data = new String[3];
 
     public String getName() {
         return _data[0];
