ae60cf178108c770a30e673bd84e7d3f8039f18d
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 11:17:34 +0800

    step3

--- src/cn/windssoft/test730/TelephoneNumber.java
+++ src/cn/windssoft/test730/TelephoneNumber.java
@@ -4,4 +4,13 @@ package cn.windssoft.test730;
  * Created by Administrator on 2017/6/12.
  */
 public class TelephoneNumber {
+    private String _areaCode;
+
+    String getAreaCode() {
+        return _areaCode;
+    }
+
+    void setAreaCode(String arg) {
+        _areaCode = arg;
+    }
 }
