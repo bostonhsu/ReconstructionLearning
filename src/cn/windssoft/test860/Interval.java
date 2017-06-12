8992bf2ecdf1184b593ec9bcda0ea97350df5bbb
徐智勇 <bostonhsu@outlook.com>
2017-06-13 06:08:20 +0800

    next

--- src/cn/windssoft/test860/Interval.java
+++ src/cn/windssoft/test860/Interval.java
@@ -7,4 +7,14 @@ import java.util.Observable;
  */
 public class Interval extends Observable {
     private String _end = "0";
+
+    String getEnd() {
+        return  _end;
+    }
+
+    void setEnd(String arg) {
+        _end = arg;
+        setChanged();
+        notifyObservers();
+    }
 }
