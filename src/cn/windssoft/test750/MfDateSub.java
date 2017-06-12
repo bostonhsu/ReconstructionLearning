d8b4a3f35a3a197de6b6fc9f264146c42d4849a1
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 14:43:03 +0800

    sub class method ok.

--- src/cn/windssoft/test750/MfDateSub.java
+++ src/cn/windssoft/test750/MfDateSub.java
@@ -1,10 +1,21 @@
 package cn.windssoft.test750;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 /**
  * Created by Administrator on 2017/6/12.
  */
 public class MfDateSub extends Date {
-    
+    public MfDateSub(String dateString) {
+        super(Date.parse(dateString));
+    }
+
+    public MfDateSub(Date arg) {
+        super(arg.getTime());
+    }
+
+    Date nextDay() {
+        return new Date(getYear(), getMonth(), getDay() + 1);
+    }
 }
