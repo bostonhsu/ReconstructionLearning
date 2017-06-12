25f46f1af6d3b920a6b4b33c746ed03603811f88
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 10:38:23 +0800

    done

--- src/cn/windssoft/test72/Account.java
+++ src/cn/windssoft/test72/Account.java
@@ -7,6 +7,6 @@ public class Account {
     private AccountType _type;
 
     double interestForAmount_days(double amount, int days) {
-        return _interestRate * amount * days / 365;
+        return _type.get_interestRate() * amount * days / 365;
     }
 }
