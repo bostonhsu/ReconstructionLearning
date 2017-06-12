ab9c81be7d79114e27f107629570aac86d2bf252
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 11:01:24 +0800

    prepare ok.

---
+++ src/cn/windssoft/test730/Person.java
@@ -0,0 +1,34 @@
+package cn.windssoft.test730;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Person {
+    private String _name;
+    private String _officeAreaCode;
+    private String _officeNumber;
+
+    public String getName() {
+        return _name;
+    }
+
+    public String getTelephoneNumber() {
+        return ("(" + _officeAreaCode + ") " + _officeNumber);
+    }
+
+    String getOfficeAreaCode() {
+        return _officeAreaCode;
+    }
+
+    void setOfficeAreaCode(String arg) {
+        _officeAreaCode = arg;
+    }
+
+    String getOfficeNumber() {
+        return _officeNumber;
+    }
+
+    void setOfficeNumber(String arg) {
+        _officeNumber = arg;
+    }
+}
