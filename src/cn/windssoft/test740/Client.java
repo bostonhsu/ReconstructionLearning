b13613f90fa150c7bf349d6e63645c9881fa099b
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 11:52:56 +0800

    所有使用了TelephoneNumber的用户，就是这个。

---
+++ src/cn/windssoft/test740/Client.java
@@ -0,0 +1,11 @@
+package cn.windssoft.test740;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Client {
+    public void test() {
+        Person martin = new Person();
+        martin.getOfficeTelephone().setAreaCode("781");
+    }
+}
