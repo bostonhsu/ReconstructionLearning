6ac9759f7db0581a4f439d4ecff9c58315b582d4
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 16:43:47 +0800

    next

---
+++ src/cn/windssoft/test810/Customer.java
@@ -0,0 +1,17 @@
+package cn.windssoft.test810;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Customer {
+    private final String _name;
+
+    public Customer(String name) {
+        _name = name;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+}
