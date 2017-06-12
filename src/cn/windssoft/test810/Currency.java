39c1d5166796634b15449c91a811a5169fb83a99
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 17:14:09 +0800

    prepare

---
+++ src/cn/windssoft/test810/Currency.java
@@ -0,0 +1,17 @@
+package cn.windssoft.test810;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class Currency {
+
+    private String _code;
+
+    public Currency(String code) {
+        _code = code;
+    }
+
+    public String getCode() {
+        return _code;
+    }
+}
