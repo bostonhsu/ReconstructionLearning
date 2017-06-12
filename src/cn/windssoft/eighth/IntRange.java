a47a177d8e4f847a891e1ee314306d5f6747bd9c
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 16:00:46 +0800

    next

--- src/cn/windssoft/eighth/IntRange.java
+++ src/cn/windssoft/eighth/IntRange.java
@@ -4,17 +4,37 @@ package cn.windssoft.eighth;
  * Created by Administrator on 2017/6/12.
  */
 public class IntRange {
+    public int getLow() {
+        return _low;
+    }
+
+    public int getHigh() {
+        return _high;
+    }
+
+    public void setLow(int low) {
+        _low = low;
+    }
+
+    public void setHigh(int high) {
+        _high = high;
+    }
+
     private int _low, _high;
 
     boolean includes(int arg) {
-        return arg >= _low && arg <= _high;
+        return arg >= getLow() && arg <= getHigh();
     }
 
     void grow(int factor) {
-        _high = _high * factor;
+        setHigh(getHigh() * factor);
     }
 
     IntRange(int low, int high) {
+        initialize(low, high);
+    }
+
+    private void initialize(int low, int high) {
         _low = low;
         _high = high;
     }
