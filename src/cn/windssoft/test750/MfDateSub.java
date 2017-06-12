c8b4366d36aeffe568caef3dfeb2c6bc62dda182
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 14:34:48 +0800

    prepare

--- src/cn/windssoft/test750/MfDateSub.java
+++ src/cn/windssoft/test750/MfDateSub.java
@@ -6,22 +6,5 @@ import java.util.Date;
  * Created by Administrator on 2017/6/12.
  */
 public class MfDateSub extends Date {
-    public MfDateSub() {
-        super();
-    }
-
-    public MfDateSub(Date arg) {
-        super(arg.getYear(), arg.getMonth(), arg.getDay() + 1);
-    }
-
-    public MfDateSub(int year, int month, int day) {
-        super(year, month, day);
-    }
-
-    public MfDateSub nextDay(Date arg) {
-        MfDateSub result = new MfDateSub(arg.getYear(), arg.getMonth(), arg.getDay() + 1);
-        return  result;
-    }
-
-
+    
 }
