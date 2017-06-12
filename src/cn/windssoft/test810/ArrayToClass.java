28f8cce8c13eba1b785fb0e3dc2eac91d1dbed77
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 17:25:18 +0800

    prepare

---
+++ src/cn/windssoft/test810/ArrayToClass.java
@@ -0,0 +1,18 @@
+package cn.windssoft.test810;
+
+/**
+ * Created by Administrator on 2017/6/12.
+ */
+public class ArrayToClass {
+    String[] row = new String[3];
+
+    // usage:
+    /*
+    row[0] = "Liverpool";
+    row[1] = "15";
+
+    String name = row[0];
+    int wins = Integer.parseInt(row[1]);
+
+     */
+}
