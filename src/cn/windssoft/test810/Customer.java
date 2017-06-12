ccddecd1fd7546db282dc8d44219744f024abe16
Boston Hsu <bostonhsu@outlook.com>
2017-06-12 16:59:05 +0800

    next

--- src/cn/windssoft/test810/Customer.java
+++ src/cn/windssoft/test810/Customer.java
@@ -1,11 +1,16 @@
 package cn.windssoft.test810;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 /**
  * Created by Administrator on 2017/6/12.
  */
 public class Customer {
     private final String _name;
 
+    private static Dictionary _instances = new Hashtable();
+    
     private Customer(String name) {
         _name = name;
     }
