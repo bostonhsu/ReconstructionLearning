fa37c9a862155afb0e062faf9151edbaeb0a7125
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 10:56:25 +0800

    next

---
+++ src/cn/windssoft/test815/Engineer.java
@@ -0,0 +1,11 @@
+package cn.windssoft.test815;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+public class Engineer extends EmployeeType {
+    @Override
+    int getTypeCode() {
+        return Employee.ENGINEER;
+    }
+}
