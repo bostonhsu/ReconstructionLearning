65771c17dfee4be4e38072b6fe7e4f501f817e58
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 10:57:22 +0800

    next

---
+++ src/cn/windssoft/test815/Salesman.java
@@ -0,0 +1,11 @@
+package cn.windssoft.test815;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+public class Salesman extends EmployeeType {
+    @Override
+    int getTypeCode() {
+        return Employee.SALESMAN;
+    }
+}
