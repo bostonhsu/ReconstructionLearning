c7ba303f9b1bbaeec8f4bdbd82718f072d1724a5
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 15:46:57 +0800

    next

--- src/cn/windssoft/test1111/MyStack.java
+++ src/cn/windssoft/test1111/MyStack.java
@@ -5,16 +5,16 @@ import java.util.Vector;
 /**
  * Created by Administrator on 2017/6/13.
  */
-public class MyStack extends Vector {
-    private Vector _vector = this;
+public class MyStack {
+    private Vector _vector = new Vector();
 
     public void push(Object element) {
         _vector.insertElementAt(element,0);
     }
 
     public Object pop() {
-        Object result = firstElement();
-        removeElementAt(0);
+        Object result = _vector.firstElement();
+        _vector.removeElementAt(0);
         return result;
     }
 }
