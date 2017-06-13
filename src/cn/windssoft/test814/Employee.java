cc8d139dba4548b4be1aeaf60a02badd227b2c45
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 10:32:09 +0800

    prepare 8.14

---
+++ src/cn/windssoft/test814/Employee.java
@@ -0,0 +1,17 @@
+package cn.windssoft.test814;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+public class Employee {
+    private int _type;
+    static final int ENGINEER = 0;
+    static final int SALESMAN = 1;
+    static final int MANAGER = 2;
+
+    Employee(int type) {
+        _type = type;
+    }
+
+
+}
