2b50ea626aa6b1b2e35b80cbceef35a3cf7c3a88
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 08:18:16 +0800

    prepare 870

---
+++ src/test870/Customer.java
@@ -0,0 +1,11 @@
+package test870;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+public class Customer {
+
+}
---
+++ src/test870/Order.java
@@ -0,0 +1,16 @@
+package test870;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+public class Order {
+    Customer getCustomer() {
+        return _customer;
+    }
+
+    void setCustomer(Customer arg) {
+        _customer = arg;
+    }
+
+    Customer _customer;
+}
