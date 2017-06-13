ba9545c3448af6f3d0747526dc6c4e153fab76ea
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 08:26:17 +0800

    next

--- src/test870/Order.java
+++ src/test870/Order.java
@@ -9,7 +9,13 @@ public class Order {
     }
 
     void setCustomer(Customer arg) {
+        if (_customer != null) {
+            _customer.friendOrders().remove(this);
+        }
         _customer = arg;
+        if (_customer != null) {
+            _customer.friendOrders().add(this);
+        }
     }
 
     Customer _customer;
