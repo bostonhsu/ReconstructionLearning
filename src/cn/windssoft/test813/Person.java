e8cf436b70d5c501bc8f1dab69efbce3dd3c109c
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 10:26:36 +0800

    remove the 残余。

--- src/cn/windssoft/test813/Person.java
+++ src/cn/windssoft/test813/Person.java
@@ -4,16 +4,7 @@ package cn.windssoft.test813;
  * Created by Administrator on 2017/6/13.
  */
 public class Person {
-    public static final int O = BloodGroup.O.getCode();
-    public static final int A = BloodGroup.A.getCode();
-    public static final int B = BloodGroup.B.getCode();
-    public static final int AB = BloodGroup.AB.getCode();
-
     private BloodGroup _bloodGroup;
-
-    public Person(int bloodGroup) {
-        _bloodGroup = BloodGroup.code(bloodGroup);
-    }
 
     public Person(BloodGroup bloodGroup) {
         _bloodGroup = bloodGroup;
@@ -21,14 +12,6 @@ public class Person {
 
     public void setBloodGroup(BloodGroup arg) {
         _bloodGroup = arg;
-    }
-
-    public void setBloodGroup(int arg) {
-        _bloodGroup = BloodGroup.code(arg);
-    }
-
-    public int getBloodGroupCode() {
-        return _bloodGroup.getCode();
     }
 
     public BloodGroup getBloodGroup() {
