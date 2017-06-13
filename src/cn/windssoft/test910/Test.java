e38bcf2777b17f2dabd2c256f1fd97def9fe43e9
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 11:47:53 +0800

    prepare

--- src/cn/windssoft/test910/Test.java
+++ src/cn/windssoft/test910/Test.java
@@ -60,4 +60,23 @@ public class Test {
     private boolean onVacation() {
         return false;
     }
+
+    public void checkSecurity(String[] people) {
+        boolean found = false;
+        for (int i = 0; i < people.length; i++) {
+            if (!found) {
+                if (people[i].equals("Don")) {
+                    sendAlert();
+                    found = true;
+                }
+                if (people[i].equals("John")) {
+                    sendAlert();
+                    found = true;
+                }
+            }
+        }
+    }
+
+    private void sendAlert() {
+    }
 }
