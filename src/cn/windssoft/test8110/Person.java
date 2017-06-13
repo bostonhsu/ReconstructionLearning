a0e3a1aaccf2632ba8a9dbfd961e583658a9d0c5
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 09:47:04 +0800

    next

--- src/cn/windssoft/test8110/Person.java
+++ src/cn/windssoft/test8110/Person.java
@@ -1,12 +1,13 @@
 package cn.windssoft.test8110;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by Administrator on 2017/6/13.
  */
 public class Person {
-    private Set _courses;
+    private Set _courses = new HashSet();
 
     public Set getCourses() {
         return _courses;
@@ -14,6 +15,14 @@ public class Person {
 
     public void setCourses(Set arg) {
         _courses = arg;
+    }
+
+    public void addCourse(Course arg) {
+        _courses.add(arg);
+    }
+
+    public void removeCourse(Course arg) {
+        _courses.remove(arg);
     }
 
 }
