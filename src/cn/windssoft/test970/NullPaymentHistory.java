e65994d239b448c09b937401d6e2e49451401f40
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 14:10:26 +0800

    next

---
+++ src/cn/windssoft/test970/NullPaymentHistory.java
@@ -0,0 +1,10 @@
+package cn.windssoft.test970;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+public class NullPaymentHistory extends PaymentHistory {
+    int getWeeksDeliquentInLastYear() {
+        return 0;
+    }
+}
