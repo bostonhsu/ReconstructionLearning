7ef5f873503c44ee8c7769e2389f74dfe72ebb35
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 08:23:52 +0800

    next

--- src/test870/Customer.java
+++ src/test870/Customer.java
@@ -7,5 +7,10 @@ import java.util.Set;
  * Created by Administrator on 2017/6/13.
  */
 public class Customer {
-    
+    private Set _orders = new HashSet();
+
+    Set friendOrders() {
+        /** should only be used by Order when modifying the association */
+        return _orders;
+    }
 }
