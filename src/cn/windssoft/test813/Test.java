077154c0ab6e89acf2c7e47edbab599f0dbb3f0f
Boston Hsu <bostonhsu@outlook.com>
2017-06-13 10:23:12 +0800

    original

---
+++ src/cn/windssoft/test813/Test.java
@@ -0,0 +1,10 @@
+package cn.windssoft.test813;
+
+/**
+ * Created by Administrator on 2017/6/13.
+ */
+public class Test {
+    public void test() {
+        Person thePerson = new Person(Person.A);
+    }
+}
